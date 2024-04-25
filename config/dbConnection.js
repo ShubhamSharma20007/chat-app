808024d58fd54025356a983f63db0855b216381f
shubham <shubhamsharma20007@gmail.com>
2024-04-25 18:23:45 +0530

    ad

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const dbConnection = () => {
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(process.env.MONGOD_CONNECT_URI)
         .then(res => console.log('database connected successfully'))
         .catch(err => console.log(err))
 }
